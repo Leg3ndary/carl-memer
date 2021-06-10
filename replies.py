763af1e4edf02261cb678f34fbf2d6298c39751e
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-06-09 23:10:42 -0400

    Created replies.py

---
+++ replies.py
@@ -0,0 +1,38 @@
+"""Just reply shit"""
+
+# Search replies... What else 
+search_replies = {
+    "Docs": [
+        [ # Successes if that's a word
+            "You searched docs and found your answer!",
+            "You read docs and understood something.",
+            "You scrolled docs and avoided being social while finding your answer...",
+            "You did something productive with your life",
+            "You made an attempt to learn something and it worked out!"
+        ],
+        [ # Fails
+            "You procrastinated reading and decided to yell at some mods.",
+            "You got muted after being too lazy to read",
+            "You refused to read because everyone is ur servant and you can't do something so hard."
+        ]
+    ],
+    "Old Folder": [
+        [ # Success
+            "You found a dank meme",
+            "You found an old tik tok",
+            "You found a macroscript you made a while ago",
+            "You found an old file"
+        ],
+        [ # Fail
+            "You opened a troll virus that shut down your computer.",
+            "You found Free_Nitro_2017_[100% LEGIT].mp4",
+            "Your computer froze.",
+            "Your computer crashed."
+        ]
+    ],
+    "Carl": [
+        
+    ],
+    "Discord": [],
+    "Bank": []
+}
