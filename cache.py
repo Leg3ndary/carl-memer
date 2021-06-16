6814b249fae7e8df4582505beb9f5b7c10954426
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-06-15 22:12:52 -0400

    Updated Cache Functions

--- cache.py
+++ cache.py
@@ -1,5 +1,6 @@
 import os
 import time
+import image_gen as ig
 
 # Because repl sucks we need to make sure packages are installed
 try:
@@ -12,6 +13,7 @@ except:
 pymongo_client = pymongo.MongoClient(f"mongodb+srv://{os.environ['MongoUser']}:{os.environ['MongoPass']}@dankmemer.ntbr7.mongodb.net/database?retryWrites=true&w=majority")
 database = pymongo_client["users_db"]
 users = database["users"]
+banned_users = database["banned"]
 
 # User Template, can be altered
 user_template = {
@@ -73,6 +75,7 @@ shop = {
 users_inv = {}
 users_bal = {}
 users_data = {}
+users_banned = {}
 
 # Here we have all the cooldown dictionaries...
 command_cooldowns = {}
@@ -122,6 +125,10 @@ for user in users.find():
         }
     })
 
+for user in banned_users.find():
+    users_banned.update({
+        str(user["_id"]): user["reason"]
+    })
 
 # Functions that we'll actually use
 def get_price(item, amount):
@@ -160,6 +167,58 @@ def transfer_bal(_id_, action, amount=None):
     Just a simple cmd that makes stuff a bit faster"""
     pass
 
+# Creating Accounts
+async def create_account(_id_):
+    """Creating a brand new account for a new user, includes adding it to our cache"""
+    # Creating another dict to alter and use...
+    user_temp = user_template
+    user_temp["_id"] = str(_id_)
+
+    users.insert_one(user_temp)
+
+    users_inv.update({
+        user_temp["_id"]: {
+            "Rare Carl": user_temp["items"]["Rare Carl"], 
+            "Carl Coin": user_temp["items"]["Carl Coin"],
+            "Carl Medal": user_temp["items"]["Carl Medal"],
+            "Carl Trophy": user_temp["items"]["Carl Trophy"],
+            "Carl Crown": user_temp["items"]["Carl Crown"],
+            "Subway": user_temp["items"]["Subway"],
+            "Subway Coupon": user_temp["items"]["Subway Coupon"],
+            "Normie triehipp": user_temp["items"]["Normie Shell"],
+            "God triehiup": user_temp["items"]["God Shell"],
+            "carltriehpup": user_temp["items"]["Carl Shell"],
+            "Pandas Chocolate": user_temp["items"]["Pandas Chocolate"],
+            "Astys Baguette": user_temp["items"]["Astys Baguette"],
+            "Oriels Neko": user_temp["items"]["Oriels Neko"],
+            "LPBs Brick": user_temp["items"]["LPBs Brick"],
+            "Mooshs Moosh": user_temp["items"]["Mooshs Moosh"]
+        }
+    })
+    users_bal.update({
+        user["_id"]: {
+            "wallet": user_temp["wallet"],
+            "bank": user_temp["bank"],
+            "bankmax": user_temp["bankmax"]
+        }
+    })
+    users_data.update({
+        user["_id"]: {
+            "patron": user_temp["patron"],
+            "proper": user_temp["proper"],
+            "streaks": {
+                "hourly": {
+                    "streak": None,
+                    "unix": 0
+                },
+                "daily": {
+                    "streak": None,
+                    "unix": 0
+                }
+            }
+        }
+    })
+
 # Checks
 def check_time(unix_time):
     """Comparing the current unix to the time given, helps deter people who aren't using everything properly
@@ -174,22 +233,28 @@ def check_snowflake(_id_):
     """Also discord user ids are 17 or 18 digits long, if the given id isn't either tag's been altered
     We could go even deeper and use the api to see if a valid id but that would slow the API down and it might be against tos
     Idk :P"""
-    if len(str(_id_)) not in [17, 18]:
+    if len(str(_id_)) not in [17, 18]:  
         return True
     return False
 
-def check_ban(_id_):
+def check_ban(_id_): # could convert to str but its fine :3
     """Checking if user was banned for abusing api, what a loser tbh"""
-    pass    
+    if str(_id_) in users_banned:
+        ig.create_image("errors/banned.png", None, "User Banned", f"{str(_id_)} was banned from using this tag, not appealable.")
+        return True
+    return False
 
 def basic_check(_id_, unix_time):
     """Combining basic tag altering checks, might help in the future ig"""
     if check_time(unix_time) or check_snowflake(_id_) is False:
+        ig.create_image("errors/tag_altered.png", None, "Tag Altered", "Please do not alter this tag in any shape, way or form. Reimport if you think this is a mistake")
         return True
     return False
 
 def check_user(_id_):
     """Checking the user id in our db and doing stuff if we need to"""
     if str(_id_) not in dict:
+        create_account(_id_)
+        ig.create_image("finished/new_user.png", None, "Welcome!", "Please wait a few seconds as your account's being created!")
         return True
     return False
