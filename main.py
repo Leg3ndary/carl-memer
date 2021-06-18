8a11af60d0f4e3207619c415f2b914a85ebb81fe
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-06-17 23:06:15 -0400

    Removed jsonify import

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, send_file, jsonify
+from flask import Flask, send_file
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from threading import Thread
